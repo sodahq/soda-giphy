35eb003e7963e1c328005e3b6485362aaea60765
Kai <kai@sodahq.com>
2024-11-27 12:43:59 +0700

    initial commit

---
+++ lib/soda_giphy.dart
@@ -0,0 +1,4 @@
+library soda_giphy;
+
+export './src/models/index.dart';
+export './src/repositories/giphy_client.dart';
---
+++ lib/src/models/index.dart
@@ -0,0 +1,8 @@
+export './collection.dart';
+export './gif.dart';
+export './images.dart';
+export './image.dart';
+export './languages.dart';
+export './rating.dart';
+export './type.dart';
+export './user.dart';
